16be4320f271982358b666ec8a09ae2cb354ce28
wangwenbin <86559685+MATshwo@users.noreply.github.com>
2024-03-28 10:28:21 +0800

    Update generate_laplacian_res.py

--- generate_laplacian_res.py
+++ generate_laplacian_res.py
@@ -23,32 +23,17 @@ smoother = Laplacian_Smooth()
 
 def laplacian_compeat(pos,edge_index):
 
-    # for i in range(n):
-    #     print("For {}-th smoothing....".format(i+1))
-    
     pos_list = smoother.laplacian_smooth(torch.from_numpy(pos),edge_index)
-    # for i in range(len(pos_list)):
-    #     out_obj = copy.deepcopy(cloth) # 深拷贝,创建新变量副本
-    #     out_obj.v = pos_list[i]
-    #     out_obj.write("laplacian_{}.obj".format(i))
-    # 平滑20次,第10次作为LF的ground_truth,最后一次作为拉普拉斯平滑的结果用于计算Loss
     return pos_list[9],pos_list[-1]
-
 
 for file in os.listdir(HF_path)[:]:
 
     if "npz" in file :
-        # 获取初始顶点坐标
-        print(file)
         init_pos = np.load(os.path.join(HF_path,file),allow_pickle=True)["final_ground"]
         lap_pos = np.zeros_like(init_pos)
         dpos = np.zeros_like(init_pos)
         for idx in range(init_pos.shape[0])[:]:
-            # out_obj = copy.deepcopy(cloth) # 深拷贝,创建新变量副本
-            # out_obj.v = init_pos[idx]
-            # out_obj.write("laplacian_init.obj")
             lap_pos[idx],dpos[idx] = laplacian_compeat(copy.deepcopy(init_pos[idx]),data.edge_index)
-        #print(lap_pos.shape,dpos.shape)
         np.savez(os.path.join(LF_path,file),lap_pos = lap_pos,dpos = dpos)
         
 
